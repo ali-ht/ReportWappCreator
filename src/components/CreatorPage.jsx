b758baa964d7a851b878f8ca05f8992abbc4e33c
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-08-10 16:52:19 +0330

    Squashed commit of the following:
    commit 6b6f93490076cf5bd3009ddbc2ef0b4ac7518b44
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 16:40:31 2024 +0330
    
        simply refactor
    
    commit a20bc1d5da7cd140dd2ec7b2adf030de0058cf02
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 15:43:44 2024 +0330
    
        check text value not empty
    
    commit ecb42cdc39336d268a823c2ecf86b2259440eb22
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 15:37:44 2024 +0330
    
        change button status with fields
    
    commit de61ec2df3bbd6fc54741a6f4148269579ba3244
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 13:53:34 2024 +0330
    
        Make CircleIcon bigger
    
    commit d530bbd44246192a61414af518afa5d7a6837a94
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 13:51:36 2024 +0330
    
        refactor and remove css file
    
    commit 47771ff65b28bc3bcae68d6620c713a8440cc95d
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 13:46:00 2024 +0330
    
        apply correct style
    
    commit 84069cb15265e9357cbf794f70925f22718e462a
    Author: SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
    Date:   Sat Aug 10 13:35:23 2024 +0330
    
        add class when selected

--- src/components/CreatorPage.jsx
+++ src/components/CreatorPage.jsx
@@ -1,17 +1,49 @@
+import React, { useState } from "react";
 import StatusSelection from "./StatusSelection";
 import DescriptionInput from "./DescriptionInput";
 import EndButtons from "./EndButtons";
 
 export default function CreatorPage() {
+
+    const [isStausSelected, setIsStatusSelected] = useState(false);
+    const [isDescriptionWritten, setIsDescriptionWritten] = useState(false);
+    const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
+    const changeStatusState = () => {
+        setIsStatusSelected(true);
+        setEnableButton(true, isDescriptionWritten);
+    };
+
+    const changeDescriptionState = (value) => {
+        setIsDescriptionWritten(value);
+        setEnableButton(isStausSelected, value);
+    };
+
+    const setEnableButton = (statusSelected, descriptionWritten) => {
+        if (statusSelected && descriptionWritten) {
+            setIsButtonEnabled(true);
+        } else {
+            setIsButtonEnabled(false);
+        }
+    };
+
     return (
         <div>
-            <div style={{padding:'16px' , gap:'32px' , display:'grid'}}>
-                <StatusSelection/>
-                <DescriptionInput/>
+            <div style={{ padding: "16px", gap: "32px", display: "grid" }}>
+                <StatusSelection onSeted={changeStatusState} />
+                <DescriptionInput onSeted={changeDescriptionState} />
             </div>
-            <div style={{padding:'8px 12px 8px 12px' , bottom:'0px' , position:'fixed' , width:'calc(100% - 24px)' , backgroundColor:'white'}}>
-            <EndButtons title="ذخیره و انتشار" disable={false}/>
+            <div
+                style={{
+                    padding: "8px 12px 8px 12px",
+                    bottom: "0px",
+                    position: "fixed",
+                    width: "calc(100% - 24px)",
+                    backgroundColor: "white",
+                }}
+            >
+                <EndButtons title="ذخیره و انتشار" disable={!isButtonEnabled} />
             </div>
         </div>
     );
-  }
+}
